8539ff5a5b8bc9cfc8bf3c8bf183804e7f9cddd4
Kevin Burke <kev@inburke.com>
2016-04-23 19:52:52 -0700

    Replace ads@goodmorningcmc with my email

--- static/advertise/index.php
+++ static/advertise/index.php
@@ -3,7 +3,7 @@
 <div style="width:35em; padding:10px;">
 <h2>Advertise with Good Morning CMC</h2>
 
-<p>Have an event, club, birthday wish, prom date invitation, or other message you'd like to promote on Good Morning CMC? I can place your message at the top of the day's email for a very reasonable rate. For more information, please contact <a href="mailto:ads@goodmorningcmc.com">ads@goodmorningcmc.com</a>.</p>
+<p>Have an event, club, birthday wish, prom date invitation, or other message you'd like to promote on Good Morning CMC? I can place your message at the top of the day's email for a very reasonable rate. For more information, please contact <a href="mailto:kev@inburke.com">kev@inburke.com</a>.</p>
 </div>
 
 </div>
